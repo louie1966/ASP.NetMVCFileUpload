9bf83859f2b6863eb2868c15e53ecf9cc2c130fd
louie1966 <renedeleeuw66@gmail.com>
2017-08-23 21:43:35 +0200

    First Commit

---
+++ ASP.NetMVCFileUpload/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NetMVCFileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload(HttpPostedFileBase file) {
+            string path = Server.MapPath("~/UploadDir");
+            file.SaveAs(path);
+            ViewBag.Path = path;
+            return View();
+        }
+    }
+}
