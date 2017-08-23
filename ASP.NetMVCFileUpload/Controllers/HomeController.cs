cf8437dde8c7c97dd4fd8e1bf0f150349f2cf5a7
louie1966 <renedeleeuw66@gmail.com>
2017-08-23 22:21:41 +0200

    Working

--- ASP.NetMVCFileUpload/Controllers/HomeController.cs
+++ ASP.NetMVCFileUpload/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using ASP.NetMVCFileUpload.HTMLHelpers;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ASP.NetMVCFileUpload.Controllers
-{
+namespace ASP.NetMVCFileUpload.Controllers {
     public class HomeController : Controller
     {
         // GET: Home
@@ -14,11 +14,25 @@ namespace ASP.NetMVCFileUpload.Controllers
             return View();
         }
 
-        public ActionResult Upload(HttpPostedFileBase file) {
-            string path = Server.MapPath("~/UploadDir");
-            file.SaveAs(path);
-            ViewBag.Path = path;
-            return View();
+        public ActionResult Upload() {
+            var r = new List<ViewDataUploadFilesResult>();
+
+            foreach (string file in Request.Files) {
+                HttpPostedFileBase hpf = Request.Files[file] as HttpPostedFileBase;
+                if (hpf.ContentLength == 0)
+                    continue;
+                string savedFileName = Path.Combine(
+                   AppDomain.CurrentDomain.BaseDirectory,
+                   "UploadDir/",
+                   Path.GetFileName(hpf.FileName));
+                hpf.SaveAs(savedFileName);
+
+                r.Add(new ViewDataUploadFilesResult() {
+                    Name = savedFileName,
+                    Length = hpf.ContentLength
+                });
+            }
+            return View("Upload", r);
         }
     }
 }
---
+++ ASP.NetMVCFileUpload/HTMLHelpers/ViewDataUploadFilesResult.cs
@@ -0,0 +1,6 @@
+﻿namespace ASP.NetMVCFileUpload.HTMLHelpers {
+    public class ViewDataUploadFilesResult {
+        public string Name { get; set; }
+        public int Length { get; set; }
+    }
+}
